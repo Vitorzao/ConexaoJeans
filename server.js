f8c81d4d66aa6894fc89891dc52fd53e3b9720fd
srbrenoss25-png <srbrenoss25@gmail.com>
2026-03-31 18:46:00 -0300

    Servidor online

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const express= require('express');
+import express from 'express'
 const app= express();
 
-app.use(express.json());
+app.get('/usuarios', (request,  res) => {
+    res.send('Ok, Deu Certo')
+})
+app.listen(4000)
