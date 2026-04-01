5e96d4bf8abd56c4bdd22b4ddbd61d577c81666c
srbrenoss25-png <srbrenoss25@gmail.com>
2026-04-01 19:22:21 -0300

    Organização das pastas css/ javascript

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 import express from 'express'
 
 const app= express();
+app.use(express.json())
 
 
 const users = []
 
 app.post('/usuarios', (req, res,) => {
 
-console.log(req)
+    users.push(req.body)
 
 res.send('Ok, aqui deu certo')
 
 })
 
-app.get('/usuarios', (request,  res) => {
-    res.send('Ok, Deu Certo')
+app.get('/usuarios', (req,  res) => {
+    res.json(users)
 })
 app.listen(4000)
